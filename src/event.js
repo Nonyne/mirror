c6eef36e4b2f0f2e70f2f2adbd3eeea48bc221ea
Nonyne <nonyne@likejs.com>
2014-10-15 10:11:01 +0800

    up 10.15

--- src/event.js
+++ src/event.js
@@ -1,7 +1,51 @@
-;(function($){
+;(function($) {
+    // event proxy
+    var createProxy = function(e) {
+            var proxy = {
+                originalEvent: event
+            };
+            Object.keys(e).forEach(function(i) {
+                proxy[i] = e[i];
+            });
+            return proxy;
+        }
+        // extend method
     $.extend($.fn, {
-        on: function(){
-            
+        on: function(event, selector, callback) {
+            var me = this;
+            var eventType = $.type(event);
+
+            if (eventType !== 'undefined') {
+                // selector
+                switch ($.type(selector)) {
+                    case 'string':
+                        if ($.type(callback) === 'function') {
+                            callback = function(selector, e) {
+                                var target = $(e.target).closest(selector).get(0);
+                                if (target) {
+                                    var evt = createProxy(e);
+                                    return this.call(target, evt);
+                                }
+                            }.bind(callback, selector);
+                        }
+                        break;
+                    case 'function':
+                        if ($.type(callback) !== 'function') {
+                            callback = selector, selector = undefined;
+                        }
+                }
+                // event binding
+                if (eventType == 'string' && callback) {
+                    event = event.split(/\s+/g);
+                    this.each(function(index) {
+                        var node = this;
+                        event.forEach(function(i) {
+                            node.addEventListener(i, callback, false);
+                        });
+                    });
+                }
+            }
+            return me;
         }
     });
 }(Mirror));
